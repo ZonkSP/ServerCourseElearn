f5e395e64c43ffd0a040115c5bf661e1f8a47fdd
ZonkSP <Ponce_ASP@outlook.com>
2023-12-05 17:29:48 -0600

    minor changes

--- routes/auth.js
+++
@@ -1,7 +0,0 @@
-const { Router } = require('express');
-const { loginUser } = require('../controllers/auth');
-const router = Router();
-
-router.post("/login", loginUser);
-
-module.exports = router;
